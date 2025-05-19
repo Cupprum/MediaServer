4f0c2242cc45189a5c2f276ed7053b4ccaf6bc45
Samuel Branisa <branisa.samuel@icloud.com>
2025-05-19 08:54:53 +0200

    chore: install helm

--- rpi/bootstrap.sh
+++ rpi/bootstrap.sh
@@ -11,8 +11,10 @@ sudo usermod -aG docker $USER
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-rm kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
+
+# Install helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 # Install minikube
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-arm64
