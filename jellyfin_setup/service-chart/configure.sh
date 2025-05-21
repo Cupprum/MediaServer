ff3be9b4b1269490f5ccd30bae8ff9d52c074958
Cupprum <branisa.samuel@icloud.com>
2025-05-21 20:18:07 +0200

    chore: actually print the new kubeconfig

--- jellyfin_setup/service-chart/configure.sh
+++ jellyfin_setup/service-chart/configure.sh
@@ -5,5 +5,8 @@ export KUBECONFIG='/etc/rancher/k3s/k3s.yaml'
 sudo chmod +r "$KUBECONFIG"
 
 echo "- Generating kubeconfig to use with kubectl from other machine"
-sed 's/127.0.0.1/192.168.0.100/g' "$KUBECONFIG"
-cat "$KUBECONFIG"
+printf -- "-%.0s" {1..50} # Print 50 dashes
+echo
+
+NEWKUBECONFIG=$(sed 's/127.0.0.1/192.168.0.100/g' "$KUBECONFIG")
+echo "$NEWKUBECONFIG"
