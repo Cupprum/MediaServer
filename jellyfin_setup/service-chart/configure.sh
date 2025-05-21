0639661ae8c3debc7a49d0f73e6d98f577bef160
Cupprum <branisa.samuel@icloud.com>
2025-05-21 20:11:48 +0200

    chore: generate kubeconfig

--- jellyfin_setup/service-chart/configure.sh
+++ jellyfin_setup/service-chart/configure.sh
@@ -3,3 +3,7 @@
 # Configure the system to use k3s
 export KUBECONFIG='/etc/rancher/k3s/k3s.yaml'
 sudo chmod +r "$KUBECONFIG"
+
+echo "- Generating kubeconfig to use with kubectl from other machine"
+sed 's/127.0.0.1/192.168.0.100/g' "$KUBECONFIG"
+cat "$KUBECONFIG"
