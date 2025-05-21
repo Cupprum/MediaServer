2568174fe1f6ba89f98ece1b0b998e739274ba84
Cupprum <branisa.samuel@icloud.com>
2025-05-21 20:54:16 +0200

    chore: move from bootstrap to cluster config

--- rpi/bootstrap.sh
+++ rpi/bootstrap.sh
@@ -8,24 +8,10 @@ sudo apt upgrade -y
 # TODO: Is this needed?
 # Install docker
 curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker $USER
-
-# Install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
-
-# Install helm
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
-# Install argocd cli
-curl -sSL -o argocd-linux-arm64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-arm64
-sudo install -m 555 argocd-linux-arm64 /usr/local/bin/argocd
-rm argocd-linux-arm64
+sudo usermod -aG docker "$USER"
 
 # Preconfigure k3s - Enable cgroup
 echo ' cgroup_enable=memory' | sudo tee -a /boot/cmdline.txt
 
 # Disable wifi
 echo "dtoverlay=disable-wifi" | sudo tee -a /boot/firmware/config.txt
-
-echo "- NOTE: Please create a new bash session to use docker without sudo."
