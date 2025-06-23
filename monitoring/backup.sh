1b8445b11d46170258f97dbeb66e23bab7b0f68b
Cupprum <branisa.samuel@icloud.com>
2025-06-24 00:22:07 +0200

    Revert "chore: add grafana and prometheus plus install dependencies"
    This reverts commit db001e94af555f7d1c371cb746b0828542429e6d.

--- monitoring/backup.sh
+++
@@ -1,5 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-
-helm dependency build
