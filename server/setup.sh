4018d8b44e0506118d62703222bb90e8b1c52a4d
Cupprum <branisa.samuel@icloud.com>
2025-06-23 18:55:18 +0200

    chore: move out of default namespace

--- server/setup.sh
+++ server/setup.sh
@@ -53,9 +53,11 @@ install_apps() {
     
     for service in "${SERVICES[@]}"; do
         log_info "Installing $service..."
-        helm install "$service" ./argocd-config-chart --set "service=$service" || {
-            log_error "Failed to install $service"
-            exit 1
+        helm install "$service" ./argocd-config-chart \
+            --namespace server \
+            --set "service=$service" || {
+                log_error "Failed to install $service"
+                exit 1
         }
     done
     
@@ -67,9 +69,10 @@ delete_apps() {
     
     for service in "${SERVICES[@]}"; do
         log_info "Removing $service..."
-        helm uninstall "$service" || {
-            log_error "Failed to remove $service"
-            exit 1
+        helm uninstall "$service" \
+            --namespace server || {
+                log_error "Failed to remove $service"
+                exit 1
         }
     done
     
