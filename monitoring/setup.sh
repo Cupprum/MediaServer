7fa5bc7041372620bbca8fb3da3ded8d77f5147f
Cupprum <branisa.samuel@icloud.com>
2025-06-23 23:12:43 +0200

    chore: helm add monitoring repos

--- monitoring/setup.sh
+++ monitoring/setup.sh
@@ -5,6 +5,11 @@ sudo chmod +r "$KUBECONFIG"
 
 # Install Prometheus
 cd './monitoring-chart' || exit 1
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
 helm dependency build
 
 helm install monitoring . \
@@ -18,3 +23,5 @@ helm uninstall monitoring \
 kubectl get secrets monitoring-grafana \
     --namespace monitoring \
     --output jsonpath="{.data.admin-password}" | base64 -d
+
+# TODO: ArgoCD
