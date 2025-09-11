6e2d81bbcf90ba96e52a67c97179580cd9c85c77
Cupprum <branisa.samuel@icloud.com>
2025-09-12 00:14:23 +0200

    test: write test of prowlarr indexers

--- server/configure-services/prowlarr_test.go
+++ server/configure-services/prowlarr_test.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"log/slog"
 	"os"
-	"strings"
 	"testing"
 )
 
@@ -12,14 +11,20 @@ type DownloadClient struct {
 	Name string `json:"name"`
 }
 
-func TestQbittorrentPresent(t *testing.T) {
+type Indexer struct {
+	Name string `json:"name"`
+}
+
+func TestMain(m *testing.M) {
 	logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelInfo,
 	}))
-	if err := getAPIKey(); err != nil {
-		t.Fatal(err)
-	}
 
+	getAPIKey()
+	m.Run()
+}
+
+func TestDownloadClients(t *testing.T) {
 	respBody, err := makeRequest("GET", prowlarrBaseURL+"/api/v1/downloadclient", nil, prowlarrHeaders)
 	if err != nil {
 		t.Fatal(err)
@@ -29,9 +34,45 @@ func TestQbittorrentPresent(t *testing.T) {
 	json.Unmarshal(respBody, &clients)
 
 	for _, client := range clients {
-		if strings.Contains(strings.ToLower(client.Name), "qbittorrent") {
-			return // Test passes
+		if client.Name == "qBittorrent" {
+			return
 		}
 	}
 	t.Error("qbittorrent download client not found in the response")
+}
+
+func TestIndexers(t *testing.T) {
+	respBody, err := makeRequest("GET", prowlarrBaseURL+"/api/v1/indexer", nil, prowlarrHeaders)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var indexerDetails []Indexer
+	json.Unmarshal(respBody, &indexerDetails)
+
+	// Expected indexer names
+	eIndexers := []string{
+		"EZTV",
+		"LimeTorrents",
+		"The Pirate Bay",
+		"YTS",
+	}
+
+	var indexers []string
+	for _, indexer := range indexerDetails {
+		indexers = append(indexers, indexer.Name)
+	}
+
+	for _, ei := range eIndexers {
+		found := false
+		for _, i := range indexers {
+			if ei == i {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Expected indexer '%s' not found in actual indexers", expectedName)
+		}
+	}
 }
