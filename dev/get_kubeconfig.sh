6d146dd70be9fab163bf23de0a5714ddbbfc07b5
Cupprum <branisa.samuel@icloud.com>
2025-10-30 15:41:15 +0100

    chore: elevate privilidges while on rpi

--- dev/get_kubeconfig.sh
+++ dev/get_kubeconfig.sh
@@ -4,5 +4,5 @@ set -e
 set -u
 set -o pipefail
 
-scp 'x42@pi.local:/etc/rancher/k3s/k3s.yaml' ~/.kube/config
+ssh 'x42@pi.local' 'sudo cat /etc/rancher/k3s/k3s.yaml' > ~/.kube/config
 sed --in-place 's/127.0.0.1/192.168.0.100/g' ~/.kube/config
