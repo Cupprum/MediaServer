ba6c8bb5d6556b5018670122a9879806b92f06e6
Cupprum <branisa.samuel@icloud.com>
2025-11-28 01:30:19 +0700

    chore: introduce flags

--- configure-services/main.go
+++ configure-services/main.go
@@ -6,21 +6,27 @@ import (
 )
 
 func main() {
-	err := ConfigureQBittorrent()
-	if err != nil {
-		fmt.Println("  * qBittorrent configuration failed:", err)
-		os.Exit(1)
+	if os.Getenv("QBITTORRENT_FLAG") == "true" {
+		err := ConfigureQBittorrent()
+		if err != nil {
+			fmt.Println("  * qBittorrent configuration failed:", err)
+			os.Exit(1)
+		}
 	}
 
-	err = ConfigureProwlarr()
-	if err != nil {
-		fmt.Println("  * Prowlarr configuration failed:", err)
-		os.Exit(1)
+	if os.Getenv("PROWLARR_FLAG") == "true" {
+		err := ConfigureProwlarr()
+		if err != nil {
+			fmt.Println("  * Prowlarr configuration failed:", err)
+			os.Exit(1)
+		}
 	}
 
-	err = ConfigureJellyfin()
-	if err != nil {
-		fmt.Println("  * Jellyfin configuration failed:", err)
-		os.Exit(1)
+	if os.Getenv("JELLYFIN_FLAG") == "true" {
+		err := ConfigureJellyfin()
+		if err != nil {
+			fmt.Println("  * Jellyfin configuration failed:", err)
+			os.Exit(1)
+		}
 	}
 }
