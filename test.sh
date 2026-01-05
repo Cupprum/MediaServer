14928fc98ca2b32b5d85b2ee857e9768b33ab95c
Cupprum <branisa.samuel@icloud.com>
2026-01-05 18:11:45 +0100

    chore: create test script to login using cookie

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 URL='http://localhost:9696'
-USER='admin'
-PASS='prowlarr'
+USER=''
+PASS=''
 COOKIE_JAR=$(mktemp)
 
 curl 'http://localhost:9696/login?returnUrl=%2F' \
-  --data-raw 'username=admin&password=prowlarr' \
+  --data-raw "username=$USER&password=$PASS" \
   -L \
   -s \
   -c "$COOKIE_JAR"
