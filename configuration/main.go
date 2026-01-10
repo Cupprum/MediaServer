210a7224571ce6e243947b9d4f5159d127787de8
Cupprum <branisa.samuel@icloud.com>
2026-01-10 12:23:52 +0100

    feat: add homepage and feature flags into main

--- configuration/main.go
+++ configuration/main.go
@@ -2,25 +2,36 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"MediaServer/configuration/homepage"
 	"MediaServer/configuration/jellyfin"
 	"MediaServer/configuration/prowlarr"
 	"MediaServer/configuration/qbittorrent"
 )
 
 func main() {
-	err := qbittorrent.Configure()
-	if err != nil {
-		log.Fatalf("--- qBittorrent configuration failed: %v\n", err)
+	if os.Getenv("QBITTORRENT_DEPLOY") == "true" {
+		if err := qbittorrent.Configure(); err != nil {
+			log.Fatalf("--- qbittorrent configuration failed: %v\n", err)
+		}
 	}
 
-	err = prowlarr.Configure()
-	if err != nil {
-		log.Fatalf("--- Prowlarr configuration failed: %v\n", err)
+	if os.Getenv("PROWLARR_DEPLOY") == "true" {
+		if err := prowlarr.Configure(); err != nil {
+			log.Fatalf("--- prowlarr configuration failed: %v\n", err)
+		}
 	}
 
-	err = jellyfin.Configure()
-	if err != nil {
-		log.Fatalf("--- Jellyfin configuration failed: %v\n", err)
+	if os.Getenv("JELLYFIN_DEPLOY") != "true" {
+		if err := jellyfin.Configure(); err != nil {
+			log.Fatalf("--- jellyfin configuration failed: %v\n", err)
+		}
+	}
+
+	if os.Getenv("HOMEPAGE_DEPLOY") == "true" {
+		if err := homepage.Configure(); err != nil {
+			log.Fatalf("--- homepage configuration failed: %v\n", err)
+		}
 	}
 }
