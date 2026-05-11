2291d86dbb8f1cc2834ab7768a7af408035253bf
Cupprum <branisa.samuel@icloud.com>
2026-05-11 19:08:31 +0200

    chore: cleanup and add logs

--- configuration/main.go
+++ configuration/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -11,25 +10,22 @@ import (
 )
 
 func main() {
-	fmt.Println(os.Getenv("MEDIASERVER_QBITTORRENT_USERNAME"))
-	fmt.Println(os.Getenv("MEDIASERVER_QBITTORRENT_RAW_PASSWORD"))
-	fmt.Println(os.Environ())
-	fmt.Println()
+	log.Println("- Starting configuration of services...")
+	log.Println()
 	if os.Getenv("MEDIASERVER_QBITTORRENT_DEPLOY") == "true" {
 		if err := qbittorrent.Configure(); err != nil {
 			log.Fatalf("--- qbittorrent configuration failed: %v\n", err)
 		}
 	}
-
 	if os.Getenv("MEDIASERVER_PROWLARR_DEPLOY") == "true" {
 		if err := prowlarr.Configure(); err != nil {
 			log.Fatalf("--- prowlarr configuration failed: %v\n", err)
 		}
 	}
-
 	if os.Getenv("MEDIASERVER_JELLYFIN_DEPLOY") == "true" {
 		if err := jellyfin.Configure(); err != nil {
 			log.Fatalf("--- jellyfin configuration failed: %v\n", err)
 		}
 	}
+	log.Println("- Finished configuration of services")
 }
